210fa643f68729ebbb292785868b6b67be9a922a
Paulo Valadares <contato@paulovaladares.com.br>
2021-03-16 00:11:45 -0300

    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    app: './main.js',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './example'),
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './example'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
