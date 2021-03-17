f5b0894145cd4ba21c2a519cc0fe7c60703139a6
Paulo Valadares <contato@paulovaladares.com.br>
2021-03-17 00:56:30 -0300

    Album list test

---
+++ src/AlbumList.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-param-reassign */
+function createMarkup(data) {
+  return data
+    .map(
+      (album) => `
+    <div class="list-item">
+      <img src="${album.images[2].url}" alt="${album.name}" lass="list-image">
+      <div class="list-description">
+        <p class="list-title">${album.name}</p>
+        <p class="list-subtitle">${album.artists[0].name}</p>
+      </div>
+    </div>`,
+    )
+    .join('');
+}
+
+export default function renderAlbum(data, element) {
+  const markup = createMarkup(data);
+  element.innerHTML = markup;
+}
