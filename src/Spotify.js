14c3ca6b5512ba52d525bc8747acff5dbae421e5
Paulo Valadares <contato@paulovaladares.com.br>
2021-03-17 01:19:45 -0300

    Rendering album data component

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,9 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
+// Remember that the token is only valid for 60 seg
 const spotify = new SpotifyWrapper({
-  token: 'dsfgsdfgfdsgdsfggdf',
+  token:
+    'BQCLElSls669WfyTGIeOsgiMyCaRo8UYfxBh89kUjS3KHAbPU1hhiPPGlZfzlznxmY-FVsHUhY777wa0miuTC-JS4zdSVgL7W5s7GQ2BHqhgpO8E-G3HmIDQq77Mun9XzBGABw-ZhWSxJ1-SCmJ6MyhMIu6XaWDVdQIX5dEdxt0KLp9fxx3apbg1gzCe4s4mlRhJgYc7-c5uoBlpR9JrnsM6K1jw8VZD208C0RGzPsz84gbc4epcjSaedZRwlY-mqXORhM1TaNWU6TBksa9bi1KalA',
 });
 
 export default spotify;
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import spotify from './Spotify';
+import renderAlbums from './AlbumList';
+
+const albums = spotify.search.albums('Incubus');
+const albumList = document.getElementById('album-list');
+
+albums.then((data) => renderAlbums(data.albums.items, albumList));
