b3b32735cdefc1932cb1a6ca38e96041a24679dd
Paulo Valadares <contato@paulovaladares.com.br>
2021-03-23 00:28:48 -0300

    Added form search

--- src/Spotify.js
+++ src/Spotify.js
@@ -3,7 +3,7 @@ import SpotifyWrapper from 'spotify-wrapper';
 // Remember that the token is only valid for 60 seg
 const spotify = new SpotifyWrapper({
   token:
-    'BQCVd4uE3rrgdDgAf9ymyUzmtFRr5_Rza0iILCntc0J8hvcEtK1DfyZMjdJNBndNNx4lurVRuXZJlO2itmfzW6gjdEPwY-8yI7GYWsybLmLGpdn1eOUqB3ZS72x2-8KsqXjVUn1f4EeRkNDCmBH5uu7u0804uWNf9PcGNVAVS1Tph-GYdi6cur8GE3i_by8D2sKM3GfRoaPcWd_I-NcFmzcUAb9EXMcq9x8RYfa4f_R3gbEmGPjzUPxXBVPBAvlNvGgiT_REAjXR6GePM31WquQWqw',
+    'BQDUU1nz_Klc_lDJ4YCMbA3wQBm42PFq4cmcGzdg9QWL41JOYySWraha8z4PBfA7-_sbQ7NddEVbwYNQkrgvz75yD-GIpo7VzN0EgCdN8blR2r2aNfBkt__K8gPuQCV0CXz5iKFRjj980wCG4r7unprDI5Yd9AWXVjFF73_dEVP0c8nD3q1jLBts91DgdLrOFNi48dmtCRzdThM9xz_kebCMKdzkuYmFkOZy9rOmby9sCk0QH2E9EsjO6R87qRuwSGKNPNmNhVxkbZH_kVvyfmRBkA',
 });
 
 export default spotify;
--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
 import spotify from './Spotify';
-import renderAlbums from './AlbumList';
 import renderAlbumInfo from './AlbumInfo';
 import renderAlbumTracks from './AlbumTracks';
+import searchEnterTrigger from './SearchTrigger';
 
-const albums = spotify.search.albums('Incubus');
 const album = spotify.album.getAlbum('6peEdPVO73WtgGah5sEhX4');
 
-const albumList = document.getElementById('album-list');
 const albumInfo = document.getElementById('album-info');
 const albumTracks = document.getElementById('album-tracks');
-
-albums.then(data => renderAlbums(data.albums.items, albumList));
 
 album
   .then(data => renderAlbumInfo(data, albumInfo))
   .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
+
+searchEnterTrigger();
